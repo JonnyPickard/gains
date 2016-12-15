19f842cf0366bdc917a59faf0a6883d94456edf9
Jonny Pickard <jonathandpickard@gmail.com>
2016-12-15 16:27:55 +0000

    Further initial layout improvements

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './app/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -20,10 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
+app.use(express.static(path.join(__dirname, './app/public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
