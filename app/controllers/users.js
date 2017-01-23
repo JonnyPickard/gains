387d2f5a90ce83ab6e31b13b251d484861212842
Jonny Pickard <jonathandpickard@gmail.com>
2017-01-23 19:45:37 +0000

    Update views to use flash error messages on invalid registration

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -15,7 +15,7 @@ router.post('/register', function(req, res) {
   let password = req.body.password;
   let password2 = req.body.password2;
 
-  // Validation
+  // Form validation
   req.checkBody('username', 'Username is required').notEmpty();
   req.checkBody('email', 'Email is required').notEmpty();
   req.checkBody('email', 'Email is not valid').isEmail();
@@ -29,27 +29,29 @@ router.post('/register', function(req, res) {
       errors: errors
     });
   } else {
-    res.redirect('/');
+    User.create(req.body, function(err) {
+      if (err) {
+        req.flash('error_msg', 'A user with these details already exists');
+        res.redirect('register');
+      } else {
+        req.flash('success_msg', 'You are registered and can now login');
+        res.redirect('/');
+      }
+    });
   }
-
-  // User.create(req.body, function(err) {
-  //   if (err) {
-  //     console.log(err);
-  //     res.redirect('/users/register');
-  //   } else {
-  //     res.redirect('/');
-  //   }
-  // });
 });
 
+// POST Logout
 router.post('/logout', function(req, res) {
   res.redirect('/users/login')
 });
 
+// GET Login
 router.get('/login', function(req, res) {
   res.render('user/login')
 });
 
+// POST Login
 router.post('/login', function(req, res) {
   res.redirect('/');
 });
