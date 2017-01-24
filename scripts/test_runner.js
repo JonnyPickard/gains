5805130fdd6643feb5975fb2be2528ba9c081c65
Jonny Pickard <jonathandpickard@gmail.com>
2017-01-24 15:00:18 +0000

    Refactored test_runner

--- scripts/test_runner.js
+++ scripts/test_runner.js
@@ -1,23 +1,25 @@
 #!/usr/bin/env node
-let path            = require("path");
-let error           = "Please specify unit or feature spec using u or f\n"
-let arg             = process.argv[2];
-let exec            = require('child_process').exec;
-let unitTestPath    = "./node_modules/mocha/bin/mocha --recursive --colors ./tests/unit";
-let featureTestPath = "./node_modules/mocha/bin/mocha --recursive --colors ./tests/e2e";
+const path            = require("path");
+const error           = "Please specify unit or feature spec using u or f\n"
+const arg             = process.argv[2];
+const exec            = require('child_process').exec;
+const unitTestPath    = "./node_modules/mocha/bin/mocha --recursive --colors ./tests/unit";
+const featureTestPath = "./node_modules/mocha/bin/mocha --recursive --colors ./tests/e2e";
 
 if (arg === "u") {
   exec("node " + unitTestPath, function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('error: '  + error);
-    console.log('stderr: ' + stderr);
+    logOutput(stdout, error, stderr)
   });
 } else if (arg === "f") {
   exec("node " + featureTestPath, function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    console.log('error: '  + error);
+    logOutput(stdout, error, stderr)
   });
 } else {
   console.error(error);
 }
+
+function logOutput(stdout, error, stderr) {
+  console.log('stdout: ' + stdout,
+              'error: '  + error,
+              'stderr: ' + stderr);
+};
