918b2587d9f689ff3ea080adac74b175bf36b43d
Jonny Pickard <jonathandpickard@gmail.com>
2017-01-25 16:48:55 +0000

    Configured mongodb for heroku

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 var config = {
 	port: process.env.PORT || 3000,
-	db: ("mongodb://localhost/gains_" + (process.env.NODE_ENV || 'development')),
+	db: process.env.MONGODB_URI || ("mongodb://localhost/gains_" + (process.env.NODE_ENV || 'development')),
 	test_db: "mongodb://localhost/gains_test",
 	production_db: "mongodb://localhost/gains_test"
 }
