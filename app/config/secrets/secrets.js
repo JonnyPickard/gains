c48f4b40e5d9fb27af59bd148af3e0a7e7d4b1c1
Jonny Pickard <jonathandpickard@gmail.com>
2017-01-26 19:51:13 +0000

    Updated secets config for heroku deployment

--- app/config/secrets/secrets.js
+++ app/config/secrets/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
-  facebookAppId: "1860458620897454",
-  facebookAppSecret: "6525b5d5c375396668aa74938c0f08dd"
+  facebookAppId: process.env.FACEBOOK_APP_ID || "1860458620897454",
+  facebookAppSecret: process.env.FACEBOOK_APP_SECRET || "6525b5d5c375396668aa74938c0f08dd"
 }
