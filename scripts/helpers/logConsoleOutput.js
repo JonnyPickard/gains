e2b78fbd30a61d0449de29b300e93c9326d1456c
Jonny Pickard <jonathandpickard@gmail.com>
2017-02-03 17:02:54 +0000

    e2e test refactoring

--- scripts/helpers/logConsoleOutput.js
+++
@@ -1,5 +0,0 @@
-module.exports = function logOutput(stdout, error, stderr) {
-  console.log('stdout: ' + stdout,
-              'error: '  + error,
-              'stderr: ' + stderr);
-};
--- tests/e2e/features/user/user_logout_spec.js
+++ tests/e2e/features/user/user_logout_spec.js
@@ -1,8 +1,8 @@
-const config   = require('../helpers/config/e2e.js');
-const User     = require('../../../app/models/user.model');
-const server   = require('../../../app');
-const signup   = require('../helpers/users/signup.js');
-const DBCleaner = require('../helpers/cleanDB');
+const config   = require('../../config/e2e.config.js');
+const User     = require('../../../../app/models/user.model');
+const server   = require('../../../../app');
+const signup   = require('../../helpers/users/signup.js');
+const DBCleaner = require('../../helpers/DB/cleanDB');
 
 module.exports = {
 
