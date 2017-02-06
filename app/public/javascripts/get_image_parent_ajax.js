11372addcc57fc7bedc2a2fcdf7ca0e7a9b095c0
Jonny Pickard <jonathandpickard@gmail.com>
2017-02-06 16:14:38 +0000

    Add ajax to get user info for each displayed photo
    Add User query that returns username and avatar
    Add GET route users/user to get details from the model
    Update index page to display avatar and user details

---
+++ app/public/javascripts/get_image_parent_ajax.js
@@ -0,0 +1,21 @@
+var getUserDetails = function(userId, tagId){
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `/users/user?userId=${userId}`);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+      if(xhr.status === 200){
+        const response = JSON.parse(xhr.responseText);
+        var ele = document.getElementById(tagId);
+        ele.innerHTML =
+        '<span><img class="user-avatar" src=' + response.avatarURL +
+        ' style="width: 60px; height: 60px; border-radius: 50%;">' +
+        '</img></span>' +
+        '<span><h3> ' + response.username + '</h3></span>';
+      }
+      else{
+        console.err('Could not get User details.');
+      }
+    }
+  };
+  xhr.send();
+};
